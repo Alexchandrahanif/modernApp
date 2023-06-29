52b4d58c3462a07a6109ff63a7484ff1c952d210
Alex Chandra Hanif <alexchandra1998@gmail.com>
2023-06-29 17:49:24 +0700

    finish

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none ${styles} active:opacity-80 hover:opacity-90 rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
--- client/src/components/FeedBackCard.jsx
+++ client/src/components/FeedBackCard.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { quotes } from "../assets";
 
-const FeedBackCard = () => {
-  return <div>FeedBackCard</div>;
-};
+const FeedBackCard = ({ content, name, title, img }) => (
+  <div
+    className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card
+  "
+  >
+    {/* Gambar bektik 2 */}
+    <img
+      src={quotes}
+      alt="double_quotes"
+      className="w-full-[42px] h-[47px] object-contain "
+    />
+    {/* Paragraph komentar */}
+    <p
+      className={`font-poppins font-normal text-[18px] leading-[32px] text-white my-10`}
+    >
+      {content}
+    </p>
+
+    {/* foto dan nama serta jabatan */}
+
+    <div className="flex flex-row items-center">
+      <div className="">
+        <img src={img} alt="foto" className="w-[48px] h-[48px] rounded-full " />
+      </div>
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white ">
+          {name}
+        </h4>
+        <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+          {title}
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 export default FeedBackCard;
